2bb125610e879a8cda112b8030ec379600032007
Jordan Lewis <jordanlewis@JordanLewis-LT.localdomain>
2022-11-01 00:31:57 +0000

    .

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const getArticles = () => {
     return axios
-      .get(`https://be-nc-news-je123.herokuapp.com/api/articles`)
+      .get(`https://backend-nc-news-jl.herokuapp.com/api/articles`)
       .then(({data}) => {
           console.log(data)
         return data;
